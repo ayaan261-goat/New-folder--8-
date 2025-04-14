eca46382c6900e139aa05586a7c878d8ded314bd
ayaan261-goat <ktm.ayaangupta@gmail.com>
2025-04-14 09:29:52 +0545

    hi

---
+++ cube.py
@@ -0,0 +1,11 @@
+def cube(num):
+    return num*num*num
+
+def by_three(number):
+    if number %3 ==0:
+        return cube (number)
+eles:
+    return false
+print(by_three(9))
+print(by_three(9))
+
